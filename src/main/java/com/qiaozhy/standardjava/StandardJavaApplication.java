1e4b6fbb875e0e1a5f038a9744e9fcff3265fc3a
qiaozhy <zhiyong.qiao@qworks.id>
2019-04-25 17:06:24 +0800

    first commit

---
+++ src/main/java/com/qiaozhy/standardjava/StandardJavaApplication.java
@@ -0,0 +1,13 @@
+package com.qiaozhy.standardjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandardJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qiaozhy/standardjava/ExceptionTest.java
@@ -0,0 +1,9 @@
+package com.qiaozhy.standardjava;
+
+/**
+ * @author: qiaozhy
+ * @Description:
+ * @Date: 2019/4/25 5:05 PM
+ */
+public class ExceptionTestgit init {
+}
